44043dc4a068429345fe979f91ee5b4586f5c100
liuhong <1002412581@qq.com>
2020-02-10 11:16:13 +0800

    Initial commit

---
+++ src/main/java/com/hongfeng/hongfeng/HongFengApplication.java
@@ -0,0 +1,13 @@
+package com.hongfeng.hongfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongFengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongFengApplication.class, args);
+	}
+
+}
